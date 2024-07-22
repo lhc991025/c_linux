70be97ddf335fc6d9d4a108d2549c2659f3bbb4b
lhc991025 <112935872+lhc991025@users.noreply.github.com>
2024-07-22 22:19:49 +0800

    Update 5_read_key.c

--- c_improve/5_read_key.c
+++ c_improve/5_read_key.c
@@ -12,32 +12,32 @@ int main(int argc, char *argv[]){
     struct input_event in_ev = {0};
     int value = 0; 
 
-    //校验传参
+    //鏍￠獙浼犲弬
     if(2 != argc ){
         fprintf("Usage: %s <input-dev>\n", argv[0]);
         exit(-1);
     }
-    //打开文件
+    //鎵撳紑鏂囦欢
     if(0 > (fd = open(argv[1], O_RDONLY))){
         perror("open");
         exit(-1);
     
     for( ; ;){
-        if(sizeof(sturct input_event) != read(fd, &in_ev, sizeof(sturct input_event))){
+        if(sizeof(struct input_event) != read(fd, &in_ev, sizeof(struct input_event))){
             perror("read");
             exit(-1);
         }
-        if(EV_KEY = in_ev.key){
-            switch(in_ev.key){
-                case 0;
-                    printf("code<%d> 松开\n"，in_ev.code);
-                    break;
-                case 1;
-                    printf("code<%d> 按下\n", in_ev.code);
-                    break;
-                case 2;
-                    printf("code<%d> 长按\n", in_ev.code);
-                    break;
+        if(EV_KEY == in_ev.type){
+            switch(in_ev.value){
+            case 0:
+                printf("code<%d> 鏉惧紑\n", in_ev.code);
+                break;
+            case 1:
+                printf("code<%d> 鎸変笅\n", in_ev.code);
+                break;
+            case 2:
+                printf("code<%d> 闀挎寜\n", in_ev.code);
+                break;
             }
         }
     }
